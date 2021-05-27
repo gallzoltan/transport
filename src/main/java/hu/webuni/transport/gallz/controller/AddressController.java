0fb290720ba686b2c304eb8baa3608f22bce1df9
Gáll Zoltán <gallzoltan@gmail.com>
2021-05-27 17:30:36 +0200

    git init

---
+++ src/main/java/hu/webuni/transport/gallz/controller/AddressController.java
@@ -0,0 +1,8 @@
+package hu.webuni.transport.gallz.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressController {
+
+}
---
+++ src/main/java/hu/webuni/transport/gallz/model/Address.java
@@ -0,0 +1,6 @@
+package hu.webuni.transport.gallz.model;
+
+
+public class Address {
+
+}
